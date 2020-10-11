ecd645dfd79cc8c0400094201af7075bb53507bd
nikolai-git-house <nikolayhouse128@gmail.com>
2020-10-11 20:35:59 +0800

    ecopay update

--- src/screens/EcoPay/index.js
+++ src/screens/EcoPay/index.js
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import AsyncStorage from '@react-native-community/async-storage';
-import {WebView} from 'react-native-webview';
-import {connect} from 'react-redux';
-import colors from '../../theme/Colors';
-import {Metrics} from '../../theme';
-import TopImage from '../../components/TopImage';
-import Logo from '../../components/Logo';
-import Firebase from '../../firebasehelper';
+import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { WebView } from "react-native-webview";
+import { connect } from "react-redux";
+import colors from "../../theme/Colors";
+import { Metrics } from "../../theme";
+import TopImage from "../../components/TopImage";
+import Logo from "../../components/Logo";
+import Firebase from "../../firebasehelper";
 import {
   saveOnboarding,
   saveInvitation,
   resetConcierge,
-} from '../../Redux/actions/index';
+} from "../../Redux/actions/index";
 
 const injectedJavascript = `(function() {
   window.postMessage = function(data) {
@@ -27,13 +22,13 @@ const injectedJavascript = `(function() {
 
 function arraytoString(array) {
   let str = "";
-  array.forEach(item => {
+  array.forEach((item) => {
     str += " " + item;
   });
   return str;
 }
 function clearAllTimeout() {
-  var id = window.setTimeout(function() {}, 0);
+  var id = window.setTimeout(function () {}, 0);
   while (id--) {
     window.clearTimeout(id); // will do nothing if no timeout with id is present
   }
@@ -43,19 +38,19 @@ class EcoPay extends React.Component {
     super(props);
     this.state = {
       pkgs: [],
-      reset: false
+      reset: false,
     };
   }
   componentDidMount() {
     let { uid, basic } = this.props;
     console.log("pkgs", this.props.basic.packages);
     if (this.props.basic.packages) {
-      let promises = this.props.basic.packages.map(item => {
-        return Firebase.getPackageInfo(item.caption).then(res => {
+      let promises = this.props.basic.packages.map((item) => {
+        return Firebase.getPackageInfo(item.caption).then((res) => {
           return res;
         });
       });
-      Promise.all(promises).then(res => {
+      Promise.all(promises).then((res) => {
         console.log("pkgs", res);
         this.setState({ pkgs: res });
       });
@@ -66,7 +61,7 @@ class EcoPay extends React.Component {
     this.unsubscribeUserProfile = Firebase.firestore()
       .collection("user")
       .doc(uid)
-      .onSnapshot(snapshot => {
+      .onSnapshot((snapshot) => {
         let userprofile = snapshot.data();
         phone = userprofile.phonenumber;
         this.props.dispatch(saveOnboarding(userprofile));
@@ -76,7 +71,7 @@ class EcoPay extends React.Component {
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     const { reset } = this.state;
-    
+
     if (reset) {
       this.restart();
     }
@@ -87,24 +82,23 @@ class EcoPay extends React.Component {
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-
     const { route } = nextProps;
 
     if (route && route.params) {
       const { reset } = route.params;
       if (reset) {
         // console.log('getDerivedStateFromProps');
-        return ({ reset }); // <- this is setState equivalent
+        return { reset }; // <- this is setState equivalent
       }
     }
-    return null
+    return null;
   }
 
   restart = async () => {
     if (this.webview) {
-      console.log('///// Reload....');
+      console.log("///// Reload....");
       this.webview.reload();
-    }    
+    }
   };
 
   navigateTo = (page, props) => {
@@ -113,27 +107,27 @@ class EcoPay extends React.Component {
 
   onLoadFinished = () => {
     let { uid, basic } = this.props;
-    console.log('/// uid', uid);
-    console.log('/// basic', basic);
+    console.log("/// uid", uid);
+    console.log("/// basic", basic);
     if (this.webview) {
       this.webview.postMessage(
         JSON.stringify({
           fromMobile: true,
-          uid: uid
+          uid: uid,
         })
       );
     }
   };
 
-  onEventHandler = data => {
-    console.log('------------- data', data);
-    if (data === 'my_ecosystem') {
-      this.navigateTo('Explore');
+  onEventHandler = (data) => {
+    console.log("------------- data", data);
+    if (data === "my_ecosystem") {
+      this.navigateTo("Explore");
     }
   };
 
   render() {
-    const { uid } = this.props;    
+    const { uid } = this.props;
 
     return (
       <View style={styles.maincontainer}>
@@ -153,14 +147,12 @@ class EcoPay extends React.Component {
         </View>
         <WebView
           style={{ zIndex: 100, marginTop: 90 }}
-          ref={r => (this.webview = r)}
+          ref={(r) => (this.webview = r)}
           originWhitelist={["*"]}
-          source={
-            Platform.OS === "ios"
-              ? { uri: "./external/ecopay/index.html" }
-              : { uri: "file:///android_asset/ecopay/index.html" }
-          }
-          onMessage={event => this.onEventHandler(event.nativeEvent.data)}
+          source={{
+            uri: `https://ecosystem.life/ecopay?uid=${uid}&headerhidden=true`,
+          }}
+          onMessage={(event) => this.onEventHandler(event.nativeEvent.data)}
           injectedJavaScript={injectedJavascript}
           startInLoadingState
           domStorageEnabled={true}
@@ -185,7 +177,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     paddingTop: 29,
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
   },
   CalltoAction: {
     paddingTop: 8,
@@ -203,7 +195,7 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.2,
-    elevation: 3
+    elevation: 3,
   },
   button: {
     color: colors.darkblue,
@@ -213,7 +205,7 @@ const styles = StyleSheet.create({
     paddingTop: 17,
     paddingBottom: 17,
     paddingLeft: 22,
-    paddingRight: 22
+    paddingRight: 22,
   },
   avatar: {
     width: 40,
@@ -225,41 +217,38 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.2,
     elevation: 3,
-    marginTop: -5
+    marginTop: -5,
   },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
   },
   buttonGroup: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
   },
   tabbutton: {
     width: 25,
     height: 25,
   },
   spinnerTextStyle: {
-    color: "#FFF"
-  }
+    color: "#FFF",
+  },
 });
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch
+    dispatch,
   };
 }
 function mapStateToProps(state) {
   return {
     basic: state.basic,
-    uid: state.uid
+    uid: state.uid,
   };
 }
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(EcoPay);
+export default connect(mapStateToProps, mapDispatchToProps)(EcoPay);
